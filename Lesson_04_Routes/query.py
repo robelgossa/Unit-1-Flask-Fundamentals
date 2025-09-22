0edec020871c96ae1a1f6ddf0f8f2fb5da4b14bb
Robel <robgos27@bergen.org>
2025-09-22 10:21:20 -0400

    template rendering

--- Lesson_04_Routes/query.py
+++ Lesson_04_Routes/query.py
@@ -1,28 +1,44 @@
 from flask import Flask, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def home():
     return f"""<h2>Query Parameters </h2
             <ul>
-                    <li><a href="/search">/search</a></li>
-                    <li><a href="/search?q=python&page=2">/search?q=python&page=2</a></li>
+                <li> Searching: 
+                    <ul> 
+                        <li><a href="/search">/search</a></li>
+                        <li><a href="/search?q=python&page=2">/search?q=python&page=2</a></li>
+                    </ul>
+                </li>
+                <li> Products: 
+                    <ul> 
+                        <li><a href="/products">/products</a></li>
+                        <li><a href="/products?page=3&max_price=100.50">/products?page=3&max_price=100.50</a></li>
+                    </ul>
+                </li>
             </ul>
 """
+
+
 @app.route("/search")
 def search():
-    query = request.args.get('q', "Nothing!")
-    page = request.args.get('page', '1')
+    query = request.args.get("q", "Nothing!")
+    page = request.args.get("page", "1")
+    return f"Searching for: '{query}' on page {page}"
 
-    return f"Searching for '{query}' on page {page}."
+
 @app.route("/products")
-def search():
-    price = request.args.get('max_price', type=float)
-    page = request.args.get('page', '1',type=int)
+def products():
+    page = request.args.get("page", "1", type=int)
+    price = request.args.get("max_price", type=float)
+    return escape(f"Page:{page} (type: {type(page)}), Max Price:{price}")
 
-    return f"Searching for '{price}' on page {page}."
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Lesson_05_Templates_Rendering/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # return 'Hello, World!'
+    return render_template('home.html')
+
+@app.route('/profile/<username>')
+def profile(username):
+    user_data = {
+        'name': username,
+        'age': 16,
+        'school': 'BT',
+        'is_present': True
+    }
+    return render_template('profile.html',user=user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
